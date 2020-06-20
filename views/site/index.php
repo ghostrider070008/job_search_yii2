ae2a7b79c394b3e95ceeb9fcd8f2c244ad3e7489
ghostrider070008 <ghostrider07008@gmail.com>
2020-06-20 09:02:02 +0300

    Сделал добавление компаний

--- views/site/index.php
+++ views/site/index.php
@@ -6,4 +6,14 @@ use yii\helpers\Url;
 
 $this->title = 'Job Search';
 ?>
-
+<div class="banner">
+    <div class="container">
+        <div class="find">
+            <h3 class="sevice">Сервис по поиску работы</h3>
+            <div class="row">
+                <input type="text" size="60" placeholder="Профессия, должность или компания...">
+                <button>Найти</button>
+            </div>
+        </div>
+    </div>
+</div><!--Футер--->
